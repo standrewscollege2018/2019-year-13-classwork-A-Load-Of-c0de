d9c656687f569290d0325b8202226fa5962509b3
Angus L <alo2326@stacmail.net>
2019-05-30 15:01:17 +1200

    First version
    Print function and variables etc

---
+++ pythonrefresher.py
@@ -0,0 +1,17 @@
+# print() is a function
+print("Hello world")
+print(25)
+
+# variables don't have a dollar sign!
+# when naming we use snake_case, so no uppercase letter or symbols other than under underscore
+first_name = "John"
+last_name = "Smith"
+
+# we can print variables or even text and variables
+print(first_name)
+print("Hello", first_name)
+# using format() is a more elegant way of combining text and variables
+print("Hello {} {}".format(first_name, last_name))
+
+
+# To get user input we use rhe input() function. Input should be assigned to a variable.
